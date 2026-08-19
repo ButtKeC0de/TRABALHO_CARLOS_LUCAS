8caa7302f4c9598f682a0c2d74aaf0e99c5937de
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-08-19 08:05:41 -0300

    UP - INICIANDO PHP DE CADASTRO

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
             <h2>Cadastro de Usuário </h2>
             <form method="POST" action="cadastro.php">
                 <label>Nome: </label>
-                <input type="text">
+                <input type="text" name="nome" id="nome">
                 <br>
                 <label>E-mail: </label>
-                <input type="password">
+                <input type="text" name="email" id="email">
             </form>
 
             <button id="submit" type="submit">ENTRAR</button>
-        </div>-
+        </div>
 
         <div class="login">
             <h2>Cadastro de Pratos</h2>
             <form method="POST" action="cadastro.php">
                 <label>Nome: </label>
-                <input type="text">
+                <input type="text" name="nome_prato id="nome_prato">
                 <br>
                 <label>Preço: </label>
-                <input type="text">
+                <input type="text" name="preco" id="preco">
                 <br>
                 <label>Categoria: </label>
-                <input type="text">
+                <input type="text" name="categoria" id="categoria">
                 <br>
                 <label>Descrição: </label>
-                <input type="password">
+                <input type="text" name="descricao" id="descricao">
 
 
             </form>
@@ -48,8 +48,24 @@
 
 
 
+    <div id="cadastro_2">
+
+
+
     </div>
 
 </body>
 
 </html>
+
+
+<?php
+
+$nome = $_POST['login'];
+$email = $_POST['email'];
+$nome_prato = $_POST['nome_prato'];
+$preco = $_POST['preco'];
+$categoria = $_POST['categoria'];
+$descricao = $_POST['descricao'];
+
+$conn = new mysqli('localhost', 'root', '', 'cadastro');
